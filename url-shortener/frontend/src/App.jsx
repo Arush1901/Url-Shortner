8416f5da460fbe3b9c40fdc24bc076385bb122cb
Arush <arushmaurya14@gmail.com>
2026-04-15 03:57:47 +0530

    first commit-6

--- url-shortener/frontend/src/App.jsx
+++ url-shortener/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const API_BASE = 'http://localhost:8080'
+const API_BASE = 'https://url-shortner-1-zynn.onrender.com'
 
 export default function App() {
   const [inputUrl, setInputUrl] = useState('')
